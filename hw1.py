e0f38b9f80bab4417a56752ce3f9554e79eebed2
Chester Jungseok Roh <chester.roh@gmail.com>
2016-12-21 18:24:32 +0900

    161221

--- hw1.py
+++ hw1.py
@@ -81,7 +81,7 @@ def load_letter(folder,min_num_images):
     for image in os.listdir(folder):
         image_file = os.path.join(folder,image)
         try:
-            image_data = ( ndimage.imread(image_file).astype(float32) - pixel_depth / 2 ) / pixel_depth
+            image_data = ( ndimage.imread(image_file).astype(float) - pixel_depth / 2 ) / pixel_depth
             if image_data.shape != (image_size,image_size):
                 raise Exception('Unexpected image shape : %s' % str(image_data.shape))
             dataset[image_index,:,:] = image_data
@@ -112,8 +112,85 @@ def maybe_pickle(data_folders,min_num_images_per_class,force=False):
                     pickle.dump(dataset,f,pickle.HIGHEST_PROTOCOL)
             except Exception as e:
                 print('unable to save data to', set_filename, ':', e)
+    return dataset_names
 
-    
+train_datasets = maybe_pickle(train_folders,45000)
+test_datasets = maybe_pickle(test_folders,1800)
+
+def disp_number_images(data_folders):
+    for folder in data_folders:
+        pickle_filename = ''.join(folder) + '.pickle'
+        try:
+            with open(pickle_filename,'rb') as f:
+                dataset = pickle.load(f)
+        except Exception as e:
+            print('Unable to read data from', pickle_filename, ':', e)
+            return
+        print('Number of images in ', folder, ':', len(dataset))
+
+disp_number_images(train_folders)
+disp_number_images(test_folders)
 
 
-        
+def make_arrays(nb_rows, img_size):
+    if nb_rows:
+        dataset = np.ndarray((nb_rows,img_size,img_size), dtype=np.float)
+        labels = np.ndarray(nb_rows,dtype=np.int32)
+    else:
+        dataset, labels = None, None
+    return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size = 0 ):
+    num_classes = len(pickle_files)
+    valid_dataset, valid_labels = make_arrays(valid_size,image_size)
+    train_dataset, train_labels = make_arrays(train_size,image_size)
+    vsize_per_class = valid_size // num_classes
+    tsize_per_class = train_size // num_classes
+
+    start_v, start_t = 0 , 0
+    end_v, end_t  = vsize_per_class, tsize_per_class
+    end_l = vsize_per_class + tsize_per_class
+
+    for label, pickle_file in enumerate(pickle_files):
+        try:
+            with open(pickle_file,'rb') as f:
+                letter_set = pickle.load(f)
+                np.random.shuffle(letter_set)
+                if valid_dataset is not None:
+                    valid_letter = letter_set[:vsize_per_class,:,:]
+                    valid_dataset[start_v:end_v,:,:] = valid_letter
+                    valid_labels[start_v:end_v] = label
+                    start_v += vsize_per_class
+                    end_v += vsize_per_class
+
+                train_letter = letter_set[vsize_per_class:end_l,:,:]
+                train_dataset[start_t:end_t,:,:] = train_letter
+                train_labels[start_t:end_t] = label
+                start_t += tsize_per_class
+                end_t += tsize_per_class
+                
+        except Exception as e:
+            print('Unable to process data from', pickle_file, ':',e)
+            raise
+
+    return valid_dataset,valid_labels,train_dataset,train_labels
+
+train_size = 300000
+valid_size = 10000
+test_size = 10000
+
+print(train_datasets)
+print(test_datasets)
+
+valid_dataset,valid_labels, train_dataset, train_labels = merge_datasets(train_datasets,train_size,valid_size)
+_,_,test_dataset,test_labels = merge_datasets(test_datasets,test_size)
+
+print('Training: ', train_dataset.shape,train_labels.shape)
+print('Validation: ', valid_dataset.shape,valid_labels.shape)
+print('Testing: ', test_dataset.shape,test_labels.shape)
+
+
+
+
+
+            
