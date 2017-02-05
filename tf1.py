81bb2338e3db9e3325771415e3b530113e3109a5
Chester Roh (ubuntu-chungdam) <chester.roh@gmail.com>
2017-02-05 14:20:24 +0900

    tf3.py added

--- tf1.py
+++ tf1.py
@@ -13,6 +13,8 @@ M = 60
 N = 30
 learning_rate = 0.003
 batch_size = 100
+training_iteration = 30
+display_step = 2
 
 sess = tf.InteractiveSession()
 
@@ -35,8 +37,6 @@ B4 = tf.Variable(tf.zeros([N]))
 W5 = tf.Variable(tf.truncated_normal([N,10],stddev=0.1))
 B5 = tf.Variable(tf.zeros([10]))
 
-sess.run(tf.global_variables_initializer())
-
 Y1 = tf.nn.relu(tf.matmul(x,W1)+B1)
 Y2 = tf.nn.relu(tf.matmul(Y1,W2)+B2)
 Y3 = tf.nn.relu(tf.matmul(Y2,W3)+B3)
@@ -44,21 +44,41 @@ Y4 = tf.nn.relu(tf.matmul(Y3,W4)+B4)
 Y4D = tf.nn.dropout(Y4,keep_prob)
 logits = tf.matmul(Y4D,W5)+B5
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=logits))
+with tf.name_scope("cost_function") as scope:
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=logits))
+    tf.scalar_summary("cost_function", cross_entropy)
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-train_step = optimizer.minimize(cross_entropy)
+with tf.name_scope("training") as scope:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_step = optimizer.minimize(cross_entropy)
 
-for i in range(8000):
-    batch = mnist.train_data.next_batch(batch_size)
-    #print("tensor ---",batch[0].shape,batch[0].dtype,batch[1].shape,batch[1].dtype)
-    sess.run(train_step, feed_dict = { x: np.reshape(batch[0],(-1,784)), y_: batch[1], keep_prob: 0.75})
+merged_summary_op = tf.merge_all_summaries()
 
-    if i % 1000 == 0:
-        print("Epoch :",i)
+sess.run(tf.global_variables_initializer())
+
+summary_writer = tf.train.SummaryWriter('./tf1_summary',graph_def=sess.graph_def)
+
+for iteration in range(training_iteration):
+    avg_cost = 0.
+    total_batch = int(mnist.train_data.data_length / batch_size)
+
+    for i in range(total_batch):
+        batch = mnist.train_data.next_batch(batch_size)
+        batch_xs = np.reshape(batch[0],(-1,784))
+        batch_ys = batch[1]
+        sess.run(train_step, feed_dict = { x: batch_xs, y_: batch_ys, keep_prob: 0.75 } )
+        
+#       avg_cost += sess.run(cross_entropy, feed_dict = { x: batch_xs, y_: batch_ys })/total_batch
+#        summary_str = sess.run(merged_summary_op, feed_dict = { x: batch_xs , y_:batch_ys})
+#       summary_writer.add_summary(summary_str, iteration * total_batch + i )
+
+    if iteration % display_step == 0:
+        print("Iteration:",'%04d' % (iteration + 1), "cost=", "{:.9f}".format(avg_cost))
+
+print("training complete")
 
 correct_prediction = tf.equal(tf.argmax(logits,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-print(sess.run(accuracy, feed_dict = { x: np.reshape(mnist.test_data.images,(-1,784)), y_: mnist.test_data.labels, keep_prob: 0.75}))
+print(sess.run(accuracy, feed_dict = { x: np.reshape(mnist.test_data.images,(-1,784)), y_: mnist.test_data.labels, keep_prob: 1}))
 
 sess.close()
--- tf3.py
+++ tf3.py
@@ -24,8 +24,11 @@ def bias_variable(shape):
     initial = tf.constant(0.1,shape=shape)
     return tf.Variable(initial)
 
-def conv2d(x,W):
+def conv2d_stride1(x,W):
     return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
+
+def conv2d_stride2(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,2,2,1],padding='SAME')
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
@@ -38,26 +41,31 @@ keep_prob = tf.placeholder(tf.float32)
 
 # first conv layer
 
-W_conv1 = weight_variable([5,5,1,32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([6,6,1,6])
+b_conv1 = bias_variable([6])
 
-h_conv1 = tf.nn.relu(conv2d(x,W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(conv2d_stride1(x,W_conv1) + b_conv1)
 
 # second conv layer
 
-W_conv2 = weight_variable([5,5,32,64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5,5,6,12])
+b_conv2 = bias_variable([12])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(conv2d_stride2(h_conv1,W_conv2) + b_conv2)
+
+# third conv layer
+
+W_conv3 = weight_variable([4,4,12,24])
+b_conv3 = bias_variable([24])
+
+h_conv3 = tf.nn.relu(conv2d_stride2(h_conv2,W_conv3)+b_conv3)
 
 # FC layer
 
-W_fc1 = weight_variable([7*7*64,1024])
+W_fc1 = weight_variable([7*7*24,1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+h_pool2_flat = tf.reshape(h_conv3,[-1,7*7*24])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
 
